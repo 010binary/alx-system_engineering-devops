ad6b3b59f82b727773eb3427da7c38c907a027f9
010binary <Augustine32fg@gmail.com>
2024-03-23 02:10:38 -0700

    ghost re

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #instaaling flask verios 2.1.0 using puppet
 
-package {'flask':
+package { 'python3':
+  ensure   => '3.8.10',
+  provider => 'apt',
+}
+
+package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
