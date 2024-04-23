cf0206bc304c53389fe3b05794e6676cabb45277
010binary <augustine32fg@gmail.com>
2024-04-23 03:14:13 +0100

    task 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Export api response to comma seperated value file (csv"""
+""" Export api response to comma seperated value file (csv)"""
 import csv
 import requests
 import sys
@@ -10,12 +10,11 @@ if __name__ == '__main__':
     res = requests.get(url_user)
     user_name = res.json().get('username')
     task = url_user + '/todos'
-    res = requests.get(task)
-    tasks = res.json()
+    tasks_response = requests.get(task)
+    tasks_data = tasks_response.json()
 
     with open('{}.csv'.format(user), 'w') as csvfile:
-        for task in tasks:
-            completed = task.get('completed')
-            title_task = task.get('title')
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, completed, title_task))
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['user', 'Username', 'Completed', 'Title'])
+        for task in tasks_data:
+            csv_writer.writerow([user, user_name, task['completed'], task['title']])
