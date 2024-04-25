64f73c766ddee555ce745c1100deaf814b79b68e
010binary <augustine32fg@gmail.com>
2024-04-24 19:33:16 -0700

    trail

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,15 +4,54 @@ import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-            ) for t in todos]
+def main():
+    """
+    main function which is the first poioint of execution for running this
+    script to get the userName, ID, title
+    """
+    # - Variables to be used
+    complete = 0
+    total = 0
+    username = ''
+    args = int(sys.argv[1])
+    userUrl = requests.get("https://jsonplaceholder.typicode.com/users")
+    todoUrl = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    userUrl.json()
+    for user in userUrl.json():
+        if user["id"] == args:
+            name = user.get("name")
+            username = user.get("username")
+
+    for todo in todoUrl.json():
+        if todo.get("userId") == args:
+            if todo.get("completed") is True:
+                complete += 1
+
+    for todo in todoUrl.json():
+        if todo.get("userId") == args:
+            if todo["completed"] is True or todo["completed"] is False:
+                total += 1
+
+    new_list = []
+    default = []
+
+    for todo in todoUrl.json():
+        if todo["userId"] == args:
+            new_list = [
+                todo["userId"],
+                username,
+                todo["completed"],
+                todo["title"]
+            ]
+            default.append(new_list)
+
+    filename = f"{args}.csv"
+    with open(filename, "w", newline="") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerows(default)
+
+
+if __name__ == "__main__":
+    main()
