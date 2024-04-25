ae3461426a334991ce173cfd2aa1f5e91224a1da
Augustine Chukwuemeka <99992586+010binary@users.noreply.github.com>
2024-04-25 04:03:49 +0100

    stan edition on export to Json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,18 +2,34 @@
 """convert res data to json"""
 import json
 import requests
-import sys
+from sys import argv
+
+
+def main():
+    """Return API data and export to CSV and JSON formats."""
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for user in users.json():
+        if user.get('id') == int(argv[1]):
+            user_id = user.get('id')
+            username = user.get('username')
+            break
+
+    tasks = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for task in todos.json():
+        if task.get('userId') == int(argv[1]):
+            task_data = {
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": username
+            }
+            tasks.append(task_data)
+
+    # Export to JSON
+    filename_json = f"{user_id}.json"
+    with open(filename_json, "w") as jsonfile:
+        json.dump({str(user_id): tasks}, jsonfile)
+
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    main()
