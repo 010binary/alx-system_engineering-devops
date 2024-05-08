200bb9f05c2da97e8590a44545b74d407fb6de13
010binary <augustine32fg@gmail.com>
2024-05-08 01:28:21 +0100

    100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces. Javascript should count as javascript,
+but java should not).
+"""
+
+
+import requests
+
+
+def count_words(subreddit, hot_list, after="", count=[]):
+    """count all words"""
+
+    if after == "":
+        count = [0] * len(hot_list)
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
+
+    if request.status_code == 200:
+        data = request.json()
+
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(hot_list)):
+                    if hot_list[i].lower() == word.lower():
+                        count[i] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(hot_list)):
+                for j in range(i + 1, len(hot_list)):
+                    if hot_list[i].lower() == hot_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
+
+            for i in range(len(hot_list)):
+                for j in range(i, len(hot_list)):
+                    if (count[j] > count[i] or
+                            (hot_list[i] > hot_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = hot_list[i]
+                        hot_list[i] = hot_list[j]
+                        hot_list[j] = aux
+
+            for i in range(len(hot_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(hot_list[i].lower(), count[i]))
+        else:
+            count_words(subreddit, hot_list, after, count)
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+# !/usr/bin/python3
+"""
+recursive function that queries the Reddit API and returns a
+list containing the titles of all hot articles for a given subreddit
+If no results are found for the given subreddit, 
+the function should return None.
+"""
+
+
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    else:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
+        
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+            for post in posts:
+                hot_list.append(post['data']['title'])
+            after = data['data']['after']
+            if after is not None:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+        else:
+            return None
+    else:
+        return None
