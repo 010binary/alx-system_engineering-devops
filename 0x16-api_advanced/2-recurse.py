db909010909a281946b7478916b1c6342e7523b3
010binary <augustine32fg@gmail.com>
2024-05-08 16:13:21 +0100

    wtf

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -27,8 +27,7 @@ def recurse(subreddit, hot_list=[], after=None):
     else:
         url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
         
-    headers = {'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
