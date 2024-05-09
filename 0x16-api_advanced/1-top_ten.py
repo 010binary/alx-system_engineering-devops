5de7aad4ccfa2e631764dee023e8353df79af1a9
010binary <augustine32fg@gmail.com>
2024-05-08 18:45:23 -0700

    top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,18 +8,20 @@ first 10 hot posts listed for a given subreddit.
 import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print("None")
-    else:
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
         print("None")
+        return
+
+    try:
+        data = response.json()
+        children = data.get("data", {}).get("children", [])
+        for child in children:
+            title = child.get("data", {}).get("title", "")
+            print(title)
+    except Exception as e:
+        print(f"Error parsing JSON: {e}")
