be758b51985eef03b657a8c90370f1a08b154f9e
010binary <augustine32fg@gmail.com>
2024-05-09 03:11:44 +0100

    trying

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,12 +11,16 @@ def number_of_subscribers(subreddit):
     Args:
         subreddit (str): the name of the subreddit
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data=response.json()
-        return data["data"]["subscribers"]
-    else:
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
