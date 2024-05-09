6fec81769350004323d8e89b00b63f8e4128c84c
010binary <augustine32fg@gmail.com>
2024-05-08 18:53:22 -0700

    top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,11 +8,11 @@ first 10 hot posts listed for a given subreddit.
 import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    print("Response status code:", response.status_code)
     if response.status_code != 200:
         print("None")
         return
